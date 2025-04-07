f842521672850ae568de971d1586a42b5bb272ca
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-07 13:45:46 -0400

    After Lunch Monday

---
+++ src/DemoSolution/DemoApi/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Up until this line (everything before this) is configuring the backend stuff in our API.
+var app = builder.Build();
+// Everything after this line is configuring "Middleware" - specificially how should HTTP Requests be mapped to our code.
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/tacos", () => "Lunch Time!");
+
+app.Run();
+
+
